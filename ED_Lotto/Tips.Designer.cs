b933ec37744c0db318f37a42612a413ac20df534
Tiranu91 <martin.schaefer.ms@outlook.de>
2018-01-25 14:37:55 +0100

    Lotto now works with array to compate more than one number.

--- ED_Lotto/Tips.Designer.cs
+++ ED_Lotto/Tips.Designer.cs
@@ -30,14 +30,26 @@
         {
             this.btn_tip = new System.Windows.Forms.Button();
             this.lbl_result = new System.Windows.Forms.Label();
-            this.tb_tip = new System.Windows.Forms.TextBox();
+            this.tb_tip0 = new System.Windows.Forms.TextBox();
+            this.tb_tip5 = new System.Windows.Forms.TextBox();
+            this.tb_tip4 = new System.Windows.Forms.TextBox();
+            this.tb_tip3 = new System.Windows.Forms.TextBox();
+            this.tb_tip2 = new System.Windows.Forms.TextBox();
+            this.tb_tip1 = new System.Windows.Forms.TextBox();
+            this.lbl_number1 = new System.Windows.Forms.Label();
+            this.lbl_number2 = new System.Windows.Forms.Label();
+            this.lbl_number6 = new System.Windows.Forms.Label();
+            this.lbl_number5 = new System.Windows.Forms.Label();
+            this.lbl_number4 = new System.Windows.Forms.Label();
+            this.lbl_number3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btn_tip
             // 
-            this.btn_tip.Location = new System.Drawing.Point(70, 89);
+            this.btn_tip.Location = new System.Drawing.Point(101, 85);
+            this.btn_tip.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_tip.Name = "btn_tip";
-            this.btn_tip.Size = new System.Drawing.Size(75, 43);
+            this.btn_tip.Size = new System.Drawing.Size(64, 35);
             this.btn_tip.TabIndex = 0;
             this.btn_tip.Text = "Tip abgeben";
             this.btn_tip.UseVisualStyleBackColor = true;
@@ -46,28 +58,141 @@
             // lbl_result
             // 
             this.lbl_result.AutoSize = true;
-            this.lbl_result.Location = new System.Drawing.Point(85, 160);
+            this.lbl_result.Location = new System.Drawing.Point(108, 134);
+            this.lbl_result.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl_result.Name = "lbl_result";
-            this.lbl_result.Size = new System.Drawing.Size(64, 17);
+            this.lbl_result.Size = new System.Drawing.Size(48, 13);
             this.lbl_result.TabIndex = 1;
             this.lbl_result.Text = "Ergebnis";
             // 
-            // tb_tip
+            // tb_tip0
             // 
-            this.tb_tip.Location = new System.Drawing.Point(59, 36);
-            this.tb_tip.Name = "tb_tip";
-            this.tb_tip.Size = new System.Drawing.Size(100, 22);
-            this.tb_tip.TabIndex = 2;
-            this.tb_tip.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tb_tip0.Location = new System.Drawing.Point(32, 28);
+            this.tb_tip0.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tb_tip0.Name = "tb_tip0";
+            this.tb_tip0.Size = new System.Drawing.Size(30, 20);
+            this.tb_tip0.TabIndex = 2;
+            this.tb_tip0.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tb_tip5
+            // 
+            this.tb_tip5.Location = new System.Drawing.Point(203, 28);
+            this.tb_tip5.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_tip5.Name = "tb_tip5";
+            this.tb_tip5.Size = new System.Drawing.Size(30, 20);
+            this.tb_tip5.TabIndex = 10;
+            this.tb_tip5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tb_tip4
+            // 
+            this.tb_tip4.Location = new System.Drawing.Point(169, 28);
+            this.tb_tip4.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_tip4.Name = "tb_tip4";
+            this.tb_tip4.Size = new System.Drawing.Size(30, 20);
+            this.tb_tip4.TabIndex = 9;
+            this.tb_tip4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tb_tip3
+            // 
+            this.tb_tip3.Location = new System.Drawing.Point(135, 28);
+            this.tb_tip3.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_tip3.Name = "tb_tip3";
+            this.tb_tip3.Size = new System.Drawing.Size(30, 20);
+            this.tb_tip3.TabIndex = 8;
+            this.tb_tip3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tb_tip2
+            // 
+            this.tb_tip2.Location = new System.Drawing.Point(101, 28);
+            this.tb_tip2.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_tip2.Name = "tb_tip2";
+            this.tb_tip2.Size = new System.Drawing.Size(30, 20);
+            this.tb_tip2.TabIndex = 7;
+            this.tb_tip2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tb_tip1
+            // 
+            this.tb_tip1.Location = new System.Drawing.Point(67, 28);
+            this.tb_tip1.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_tip1.Name = "tb_tip1";
+            this.tb_tip1.Size = new System.Drawing.Size(30, 20);
+            this.tb_tip1.TabIndex = 6;
+            this.tb_tip1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lbl_number1
+            // 
+            this.lbl_number1.AutoSize = true;
+            this.lbl_number1.Location = new System.Drawing.Point(39, 50);
+            this.lbl_number1.Name = "lbl_number1";
+            this.lbl_number1.Size = new System.Drawing.Size(13, 13);
+            this.lbl_number1.TabIndex = 11;
+            this.lbl_number1.Text = "1";
+            // 
+            // lbl_number2
+            // 
+            this.lbl_number2.AutoSize = true;
+            this.lbl_number2.Location = new System.Drawing.Point(74, 50);
+            this.lbl_number2.Name = "lbl_number2";
+            this.lbl_number2.Size = new System.Drawing.Size(13, 13);
+            this.lbl_number2.TabIndex = 12;
+            this.lbl_number2.Text = "2";
+            // 
+            // lbl_number6
+            // 
+            this.lbl_number6.AutoSize = true;
+            this.lbl_number6.Location = new System.Drawing.Point(210, 50);
+            this.lbl_number6.Name = "lbl_number6";
+            this.lbl_number6.Size = new System.Drawing.Size(13, 13);
+            this.lbl_number6.TabIndex = 13;
+            this.lbl_number6.Text = "6";
+            // 
+            // lbl_number5
+            // 
+            this.lbl_number5.AutoSize = true;
+            this.lbl_number5.Location = new System.Drawing.Point(177, 50);
+            this.lbl_number5.Name = "lbl_number5";
+            this.lbl_number5.Size = new System.Drawing.Size(13, 13);
+            this.lbl_number5.TabIndex = 14;
+            this.lbl_number5.Text = "5";
+            // 
+            // lbl_number4
+            // 
+            this.lbl_number4.AutoSize = true;
+            this.lbl_number4.Location = new System.Drawing.Point(142, 50);
+            this.lbl_number4.Name = "lbl_number4";
+            this.lbl_number4.Size = new System.Drawing.Size(13, 13);
+            this.lbl_number4.TabIndex = 15;
+            this.lbl_number4.Text = "4";
+            // 
+            // lbl_number3
+            // 
+            this.lbl_number3.AutoSize = true;
+            this.lbl_number3.Location = new System.Drawing.Point(108, 50);
+            this.lbl_number3.Name = "lbl_number3";
+            this.lbl_number3.Size = new System.Drawing.Size(13, 13);
+            this.lbl_number3.TabIndex = 16;
+            this.lbl_number3.Text = "3";
             // 
             // Tips
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(219, 217);
-            this.Controls.Add(this.tb_tip);
+            this.ClientSize = new System.Drawing.Size(280, 176);
+            this.Controls.Add(this.lbl_number3);
+            this.Controls.Add(this.lbl_number4);
+            this.Controls.Add(this.lbl_number5);
+            this.Controls.Add(this.lbl_number6);
+            this.Controls.Add(this.lbl_number2);
+            this.Controls.Add(this.lbl_number1);
+            this.Controls.Add(this.tb_tip5);
+            this.Controls.Add(this.tb_tip4);
+            this.Controls.Add(this.tb_tip3);
+            this.Controls.Add(this.tb_tip2);
+            this.Controls.Add(this.tb_tip1);
+            this.Controls.Add(this.tb_tip0);
             this.Controls.Add(this.lbl_result);
             this.Controls.Add(this.btn_tip);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Tips";
             this.Text = "Tips";
             this.ResumeLayout(false);
@@ -79,6 +204,17 @@
 
         private System.Windows.Forms.Button btn_tip;
         private System.Windows.Forms.Label lbl_result;
-        private System.Windows.Forms.TextBox tb_tip;
+        private System.Windows.Forms.TextBox tb_tip0;
+        private System.Windows.Forms.TextBox tb_tip5;
+        private System.Windows.Forms.TextBox tb_tip4;
+        private System.Windows.Forms.TextBox tb_tip3;
+        private System.Windows.Forms.TextBox tb_tip2;
+        private System.Windows.Forms.TextBox tb_tip1;
+        private System.Windows.Forms.Label lbl_number1;
+        private System.Windows.Forms.Label lbl_number2;
+        private System.Windows.Forms.Label lbl_number6;
+        private System.Windows.Forms.Label lbl_number5;
+        private System.Windows.Forms.Label lbl_number4;
+        private System.Windows.Forms.Label lbl_number3;
     }
 }
